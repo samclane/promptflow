545506a958f19e70c45d825f8db3296f0a639c7e
Sawyer McLane <samclane@gmail.com>
2023-05-08 10:16:36 -0600

    File input node

--- promptflow/src/nodes/input_node.py
+++ promptflow/src/nodes/input_node.py
@@ -1,8 +1,10 @@
 """
 Nodes that get run time input from the user
 """
-import tkinter
+import tkinter as tk
+from typing import Any
 import customtkinter
+from promptflow.src.dialogues.multi_file import MultiFileInput
 
 from promptflow.src.nodes.node_base import NodeBase
 
@@ -20,3 +22,34 @@ class InputNode(NodeBase):
 
     def run_subclass(self, before_result, state, console):
         return before_result["input"]
+
+
+class FileInput(NodeBase):
+    """
+    Reads a file and returns its contents
+    """
+
+    options_popup: MultiFileInput = None
+    filename: str = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.filename = kwargs.get("filename", "")
+
+    def edit_options(self, event):
+        self.options_popup = MultiFileInput(
+            self.canvas,
+            {
+                "filename": self.filename,
+            },
+        )
+        self.canvas.wait_window(self.options_popup)
+        if self.options_popup.cancelled:
+            return
+        self.filename = self.options_popup.result["filename"]
+
+    def run_subclass(
+        self, before_result: Any, state, console: tk.scrolledtext.ScrolledText
+    ) -> str:
+        with open(self.filename, "r", encoding="utf-8") as f:
+            return f.read()
