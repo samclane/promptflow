da385affc7000ca5bae6e72116a5df5ef95e7d2a
Sawyer McLane <samclane@gmail.com>
2023-05-26 10:17:33 -0600

    Escape curly braces from web content

--- promptflow/src/nodes/http_node.py
+++ promptflow/src/nodes/http_node.py
@@ -245,6 +245,8 @@ class ScrapeNode(NodeBase):
         for script in soup(["script", "style"]):
             script.extract()
         text = soup.get_text()
+        # escape curly braces
+        text = text.replace("{", "{{").replace("}", "}}")
         lines = [line.strip() for line in text.splitlines()]
         chunks = [phrase.strip() for line in lines for phrase in line.split("  ")]
         text = "\n".join(chunk for chunk in chunks if chunk)
