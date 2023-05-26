c208b066cfbf95a481ef4ff195b5b198f22a8626
Sawyer McLane <samclane@gmail.com>
2023-05-26 08:01:12 -0600

    Image Inspector Zoom

--- promptflow/src/dialogues/image_inspector.py
+++ promptflow/src/dialogues/image_inspector.py
@@ -2,7 +2,7 @@
 Display image in a dialogue box
 """
 
-from PIL import Image, ImageTk
+from PIL import ImageTk
 
 import customtkinter
 
@@ -17,9 +17,6 @@ class ImageInspector(customtkinter.CTkToplevel):
         self.master = master
         self.image = image
         self.title("Image Inspector")
-        self.body()
-
-    def body(self):
         self.canvas = customtkinter.CTkCanvas(
             self, width=self.image.width(), height=self.image.height()
         )
@@ -27,6 +24,12 @@ class ImageInspector(customtkinter.CTkToplevel):
         self.canvas.create_image(0, 0, image=self.image, anchor="nw")
         self.canvas.update()
         self.grab_set()
+        # bind scroll wheel to zoom
+        self.canvas.bind("<MouseWheel>", self.handle_zoom)  # Windows
+        self.canvas.bind("<Button-4>", self.handle_zoom)  # Linux (wheel up)
+        self.canvas.bind("<Button-5>", self.handle_zoom)  # Linux (wheel down)
+        self.canvas.bind("<4>", self.handle_zoom)  # MacOS (wheel up)
+        self.canvas.bind("<5>", self.handle_zoom)  # MacOS (wheel down)
 
     def buttonbox(self):
         pass
@@ -34,3 +37,16 @@ class ImageInspector(customtkinter.CTkToplevel):
     def destroy(self):
         super().destroy()
         self.image = None
+
+    def handle_zoom(self, event):
+        """
+        Zooms in or out of the image
+        """
+        self.canvas.scale(
+            "all",
+            event.x,
+            event.y,
+            1 + event.delta / 1200,
+            1 + event.delta / 1200,
+        )
+        self.canvas.update()
