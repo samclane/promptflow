ff6dfdc7b6c0ae42e5aaf445978b4576eac251c3
Sawyer McLane <samclane@gmail.com>
2023-06-26 08:37:26 -0600

    NOT NULL DB entries

--- promptflow/sql/initialize_schema.sql
+++ promptflow/sql/initialize_schema.sql
@@ -3,37 +3,35 @@ PRAGMA foreign_keys = ON;
 
 -- Graphs
 CREATE TABLE IF NOT EXISTS graphs (
-    id SERIAL PRIMARY KEY,
-    name TEXT UNIQUE,
-    created TIMESTAMP
+    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    name TEXT UNIQUE NOT NULL,
+    created TIMESTAMP NOT NULL
 );
 
 -- Node Types
 CREATE TABLE IF NOT EXISTS node_types (
-    id SERIAL PRIMARY KEY,
-    metadata JSON,
-    name TEXT UNIQUE
+    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    metadata JSON NOT NULL,
+    name TEXT UNIQUE NOT NULL
 );
 
 -- Nodes
 CREATE TABLE IF NOT EXISTS nodes (
-    id SERIAL PRIMARY KEY,
-    node_type_id INTEGER,
-    graph_id INTEGER,
-    label TEXT,
+    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    node_type_id INTEGER NOT NULL,
+    graph_id INTEGER NOT NULL,
+    label TEXT NOT NULL,
     FOREIGN KEY (node_type_id) REFERENCES node_types(id),
     FOREIGN KEY(graph_id) REFERENCES graphs(id)
 );
 
-
-
 -- Branches
 CREATE TABLE IF NOT EXISTS branches (
-    id SERIAL PRIMARY KEY,
-    conditional TEXT,
-    label TEXT,
-    node INTEGER,
-    next_node INTEGER,
+    id SERIAL PRIMARY KEY UNIQUE NOT NULL,
+    conditional TEXT NOT NULL,
+    label TEXT NOT NULL,
+    node INTEGER NOT NULL,
+    next_node INTEGER NOT NULL,
     FOREIGN KEY (node) REFERENCES nodes(id),
     FOREIGN KEY (next_node) REFERENCES nodes(id)
 );
