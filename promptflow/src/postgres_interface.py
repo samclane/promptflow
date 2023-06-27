e60473ad1676e5830a757db79e19c1e3c460642f
Sawyer McLane <samclane@gmail.com>
2023-06-27 10:24:05 -0600

    Added imports for all nodes + main test code

--- promptflow/src/postgres_interface.py
+++ promptflow/src/postgres_interface.py
@@ -4,8 +4,44 @@ from typing import Optional, Dict, Any, List
 import psycopg2
 
 from promptflow.src.flowchart import Flowchart
-from promptflow.src.nodes.input_node import InputNode
-from promptflow.src.nodes.test_nodes import LoggingNode
+from promptflow.src.nodes.node_base import NodeBase
+from promptflow.src.nodes.start_node import InitNode, StartNode
+from promptflow.src.nodes.input_node import InputNode, FileInput, JSONFileInput
+from promptflow.src.nodes.func_node import FuncNode
+from promptflow.src.nodes.llm_node import OpenAINode, ClaudeNode, GoogleVertexNode
+from promptflow.src.nodes.date_node import DateNode
+from promptflow.src.nodes.random_number import RandomNode
+from promptflow.src.nodes.history_node import (
+    HistoryNode,
+    ManualHistoryNode,
+    HistoryWindow,
+    WindowedHistoryNode,
+    DynamicWindowedHistoryNode,
+)
+from promptflow.src.nodes.dummy_llm_node import DummyNode
+from promptflow.src.nodes.prompt_node import PromptNode
+from promptflow.src.nodes.embedding_node import (
+    EmbeddingInNode,
+    EmbeddingQueryNode,
+    EmbeddingsIngestNode,
+)
+from promptflow.src.nodes.test_nodes import AssertNode, LoggingNode, InterpreterNode
+from promptflow.src.nodes.env_node import EnvNode, ManualEnvNode
+from promptflow.src.nodes.audio_node import WhispersNode, ElevenLabsNode
+from promptflow.src.nodes.db_node import PGQueryNode, SQLiteQueryNode, PGGenerateNode
+from promptflow.src.nodes.structured_data_node import JsonNode, JsonerizerNode
+from promptflow.src.nodes.websearch_node import SerpApiNode, GoogleSearchNode
+from promptflow.src.nodes.output_node import FileOutput, JSONFileOutput
+from promptflow.src.nodes.http_node import HttpNode, JSONRequestNode, ScrapeNode
+from promptflow.src.nodes.server_node import ServerInputNode
+from promptflow.src.nodes.memory_node import PineconeInsertNode, PineconeQueryNode
+from promptflow.src.nodes.image_node import (
+    DallENode,
+    CaptionNode,
+    OpenImageFile,
+    JSONImageFile,
+    SaveImageNode,
+)
 
 
 class GraphView(BaseModel):
@@ -81,8 +117,9 @@ class PostgresInterface:
         )
         self.cursor = self.conn.cursor()
 
-    def get_graph_view(self) -> List[GraphView]:
-        self.cursor.execute("SELECT * FROM graph_view")
+    def get_graph_view(self) -> List[GraphView]:  # todo deprecate this method  
+        self.cursor.execute("SELECT * FROM graph_view")  # todo select id,name from graph_view  for function get_graph_view
+        # todo for function get_graph_view_to_flowchart_list select id,name from graph_view where id = input_id
         rows = self.cursor.fetchall()
 
         # Getting column names
@@ -120,7 +157,24 @@ class PostgresInterface:
             node = eval(row.node_type_name).deserialize(
                 flowchart,
                 row.node_type_metadata
-                | {"label": row.node_label, "center_x": 0, "center_y": 0},
+                | {"label": row.node_label, "center_x": 0, "center_y": 0},  # todo remove center_x and center_y
             )
             flowchart.add_node(node)
         return flowcharts
+    
+    def get_flowchart_by_id(self, id):
+        pass
+    
+    def get_all_flowchart_ids_and_names(self):
+        pass
+
+if __name__ == "__main__":
+    config = DatabaseConfig(
+        host="172.18.0.3", database="postgres", user="postgres", password="postgres"
+    )
+    postgres_interface = PostgresInterface(config)
+    graph_view = postgres_interface.get_graph_view()
+    flowcharts = postgres_interface.graph_view_to_flowchart_list(graph_view)
+    # print all nodes 
+    for flowchart in flowcharts:
+        print(flowchart.nodes)
