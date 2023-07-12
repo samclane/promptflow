9d2fdc61142bbc8ef8c4907c8e0e143351dbf8ad
Sawyer McLane <samclane@gmail.com>
2023-07-12 10:09:26 -0600

    Removed frontend files

--- frontend/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge } = require('electron')
-
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke('ping')
-  // we can also expose variables, not just functions
-})
