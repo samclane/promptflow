836ef1a8f06ec81364c82f5089f245cd21e8e09f
Sawyer McLane <samclane@gmail.com>
2023-07-17 11:14:20 -0600

    Can sort job id

--- dashboard/src/app/job-list.component.ts
+++ dashboard/src/app/job-list.component.ts
@@ -8,6 +8,7 @@ import { JobsService } from './jobs.service';
 })
 export class JobListComponent implements OnInit {
   jobs: any[] = [];
+  sortDirection = 1; // 1 for ascending, -1 for descending
 
   constructor(private jobsService: JobsService) { }
 
@@ -19,4 +20,9 @@ export class JobListComponent implements OnInit {
     this.jobsService.getJobs().subscribe(jobs => this.jobs = jobs);
   }
 
+  sortJobs(property_name: string) {
+    this.jobs.sort((a, b) => (a[property_name] > b[property_name] ? 1 : -1) * this.sortDirection);
+    // Flip the direction for the next sort
+    this.sortDirection *= -1;
+  }
 }
