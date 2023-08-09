be0af0686241770808cb970ee5bbe5d768e2e7f8
Frankie Primerano <frankieprimerano@gmail.com>
2023-08-09 14:29:46 -0400

    link to flow chart

--- dashboard/src/app/flowchart-list.component.ts
+++ dashboard/src/app/flowchart-list.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FlowchartService } from './flowchart.service';
 import {combineLatest} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-flowchart-list',
@@ -9,13 +10,20 @@ import {combineLatest} from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FlowchartListComponent {
-  constructor(private readonly flowchartService: FlowchartService) { }
+  constructor(
+    private readonly flowchartService: FlowchartService,
+    private readonly router: Router
+  ) { }
 
   private readonly flowcharts$ = this.flowchartService.flowcharts$;
 
   public readonly vm$ = combineLatest({
     flowcharts: this.flowcharts$
   });
+
+  navigateToFlowchart(uid: string): void {
+    this.router.navigate(['/flowcharts', uid]);
+  }
 
   deleteFlowchart(id: string): void {
     this.flowchartService.deleteFlowchart(id)
