ca0bb6da12ee4e479a9c64392eda6c741e0c5654
Frankie Primerano <frankieprimerano@gmail.com>
2023-08-10 13:02:38 -0400

    rxjs

--- dashboard/src/app/chat.component.ts
+++ dashboard/src/app/chat.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Message, ChatResponse } from './message';
+import {BehaviorSubject, combineLatest, map, scan, share, shareReplay, startWith, Subject, switchMap, tap, withLatestFrom} from 'rxjs';
+import {FormBuilder} from '@angular/forms';
 
+interface Message {
+  sender: 'AI' | 'USER';
+  text: string;
+  timestamp: string;
+};
+
+interface ChatResponse {
+  ai_message: Message;
+  user_message: Message;
+};
 
 @Component({
   selector: 'app-chat',
@@ -10,30 +21,59 @@ import { Message, ChatResponse } from './message';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
-  userMessages: Message[] = [];
-  aiMessages: Message[] = [];
-  allMessages: Message[] = []; // Combined array of user and AI messages
-  messageInput: string = '';
-  isLoading: boolean = false;
+  constructor(
+    private readonly http: HttpClient,
+    private readonly form: FormBuilder
+  ) {}
 
-  constructor(private http: HttpClient) {}
+  public readonly messageControl = this.form.nonNullable.control('');
+
+  private readonly loadingSource = new BehaviorSubject<boolean>(false);
+
+  private readonly messagesSource = new Subject<Message>();
+  private readonly messages$ = this.messagesSource.pipe(
+    scan((a, b) => {
+      return [...a, b];
+    }, <Message[]>[]),
+    startWith(<Message[]>[]),
+  )
+
+  private readonly sendMessageSource = new Subject<Message>();
+  private readonly sendMessage$ = this.sendMessageSource.pipe(
+    tap((x) => {
+      this.messagesSource.next(x);
+      this.loadingSource.next(true);
+    }),
+    withLatestFrom(this.messages$),
+    switchMap(([_, messages]) => this.http.post<ChatResponse>(this.buildUrl('/chat'), messages)),
+    tap((m) => {
+      this.messagesSource.next({
+        sender: 'AI',
+        text: m.ai_message.text,
+        timestamp: new Date().toISOString()
+      });
+      this.loadingSource.next(false)
+    }),
+    startWith(<Message>{ sender: 'USER', text: '', timestamp: '' }),
+  )
+
+  public readonly vm$ = combineLatest({
+    messages: this.messages$,
+    sendMessage: this.sendMessage$,
+    loading: this.loadingSource.asObservable()
+  });
 
   // Method to send a message and receive AI response
-  sendMessage(sender: string) {
-    const timestamp = new Date().toLocaleTimeString();
-    const userMessage: Message = { sender, text: this.messageInput, timestamp };
-    this.userMessages.push(userMessage);
-    this.allMessages.push(userMessage);
-    
-    this.isLoading = true;
+  sendMessage() {
+    if (!this.messageControl.value) return;
 
-    this.http.post<ChatResponse>(this.buildUrl('/chat'), this.allMessages).subscribe(response => {
-      this.aiMessages.push(response.ai_message);
-      this.allMessages.push(response.ai_message);
-      this.isLoading = false;
-    });
+    this.sendMessageSource.next({
+      sender: 'USER',
+      text: this.messageControl.value,
+      timestamp: (new Date()).toISOString()
+    })
 
-    this.messageInput = '';
+    this.messageControl.reset();
   }
 
   get apiUrl() {
