4d16206a0b0b3480d3fa413461dfbc5863d72665
Sawyer McLane <samclane@gmail.com>
2023-08-22 10:18:42 -0600

    Added sliders to chat options

--- dashboard/src/app/slider.component.ts
+++ dashboard/src/app/slider.component.ts
@@ -1,24 +1,45 @@
-import { Component, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { debounceTime } from 'rxjs/operators';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-slider',
     templateUrl: './slider.component.html',
-    styleUrls: ['./slider.component.css']
+    styleUrls: ['./slider.component.css'],
+    providers: [
+        {
+          provide: NG_VALUE_ACCESSOR,
+          useExisting: forwardRef(() => SliderComponent),
+          multi: true
+        }
+    ]
 })
-export class SliderComponent {
+export class SliderComponent implements ControlValueAccessor {
     @Input() min: number = 0;
     @Input() max: number = 100;
-    @Input() start: number = 50;
-    @Input() stepSize: number = 1;
+    @Input() stepSize: number = 0.1;
+    @Input() start = 0;
+  
     sliderControl = new FormControl(this.start);
-
+  
+    onChange: any = () => {};
+    onTouch: any = () => {};
+  
     constructor() {
-        this.sliderControl.valueChanges.pipe(
-            debounceTime(300)
-        ).subscribe(value => {
-            console.log("Slider Value:", value);
-        });
+      this.sliderControl.valueChanges.subscribe(value => {
+        this.onChange(value);
+      });
     }
-}
+  
+    writeValue(value: any): void {
+      this.sliderControl.setValue(value, { emitEvent: false });
+    }
+  
+    registerOnChange(fn: any): void {
+      this.onChange = fn;
+    }
+  
+    registerOnTouched(fn: any): void {
+      this.onTouch = fn;
+    }
+  }
+  
