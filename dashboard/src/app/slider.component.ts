1b0017d6d5b022265b159033da33f99ddec66b8f
Sawyer McLane <samclane@gmail.com>
2023-08-22 10:08:56 -0600

    added stepsize to slider for floats

--- dashboard/src/app/slider.component.ts
+++ dashboard/src/app/slider.component.ts
@@ -3,21 +3,22 @@ import { FormControl } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-slider',
-  templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+    selector: 'app-slider',
+    templateUrl: './slider.component.html',
+    styleUrls: ['./slider.component.css']
 })
 export class SliderComponent {
     @Input() min: number = 0;
     @Input() max: number = 100;
     @Input() start: number = 50;
+    @Input() stepSize: number = 1;
     sliderControl = new FormControl(this.start);
-    
-  constructor() {
-    this.sliderControl.valueChanges.pipe(
-      debounceTime(300)
-    ).subscribe(value => {
-      console.log("Slider Value:", value);
-    });
-  }
+
+    constructor() {
+        this.sliderControl.valueChanges.pipe(
+            debounceTime(300)
+        ).subscribe(value => {
+            console.log("Slider Value:", value);
+        });
+    }
 }
