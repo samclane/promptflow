8a0b830e29733ac863a375a3058f5ac6443097d1
Sawyer McLane <samclane@gmail.com>
2023-08-30 09:02:18 -0600

    Added branches to flowchart display

--- dashboard/src/app/flowchart-tree.component.ts
+++ dashboard/src/app/flowchart-tree.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, Input, OnChanges, SimpleChanges, ChangeDetectorRef, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, OnChanges, SimpleChanges, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Component({
@@ -10,7 +10,7 @@ import { BehaviorSubject } from 'rxjs';
 export class FlowchartTreeComponent implements OnInit, OnChanges {
   @Input() treeData: string | undefined | null;
   parsedJson: any;
-  constructor(private cdr: ChangeDetectorRef) {}  // <-- Inject ChangeDetectorRef
+  constructor() {}
   private _vm$ = new BehaviorSubject<any>(null);
   public readonly vm$ = this._vm$.asObservable();
 
@@ -18,12 +18,9 @@ export class FlowchartTreeComponent implements OnInit, OnChanges {
     console.log('Component initialized');
   }
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('Changes detected', changes);
     if (changes['treeData'] && changes['treeData'].currentValue) {
-      console.log("I'm here, with data:", this.treeData);
       this.parsedJson = JSON.parse(this.treeData as string);
       this._vm$.next(this.parsedJson);
-      this.cdr.markForCheck();  // <-- Manually mark for check
     }
   }
   objectKeys(obj: any): string[] {
